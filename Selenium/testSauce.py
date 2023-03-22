00646119a9c5a6cf5976361bf9c49ca7d365c033
Alperen Gökbak <swalperen3008@gmail.com>
2023-03-22 23:29:01 +0300

    Added extra function in testsauce

--- Selenium/testSauce.py
+++ Selenium/testSauce.py
@@ -77,6 +77,39 @@ class TestSauce:
         sleep(2)
         list_item = self.driver.find_elements(By.CLASS_NAME, "inventory_item")
         print(f"Number of Item: {len(list_item)}")
+    
+    def test_add_cart(self):
+        self.user_name.send_keys("standard_user")
+        self.password.send_keys("secret_sauce")
+        sleep(1)
+        login_btn = self.driver.find_element(By.CLASS_NAME, "submit-button")
+        login_btn.click()
+        sleep(1)
+        click_button = self.driver.find_element(By.XPATH, "//*[@id='add-to-cart-sauce-labs-bike-light']")
+        click_button.click()
+        sleep(1)
+        cart_button = self.driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+        cart_button.click()
+        sleep(1)
+        test_cart = self.driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
+        test_result = int(test_cart.text) > 0
+        print(f"Test Result: {test_result}, Product count: {test_cart.text}")
+        
+    def test_take_price_list(self):
+        self.user_name.send_keys("standard_user")
+        self.password.send_keys("secret_sauce")
+        sleep(1)
+        login_btn = self.driver.find_element(By.CLASS_NAME, "submit-button")
+        login_btn.click()
+        sleep(1)
+        my_list = list()
+        limit = self.driver.find_elements(By.CLASS_NAME, "inventory_item_price")
+        width = self.driver.execute_script("return arguments[0].offsetWidth", limit) # We can take the size of defined value with that function.
+        
+        for element in limit:
+            my_list.append(float(element.text[1 : width]))
+        print(my_list)
+
 
 test_Class = TestSauce()
 #test_Class.test_invalid_login()
@@ -84,4 +117,6 @@ test_Class = TestSauce()
 #test_Class.test_login_empty_password()
 #test_Class.test_login_locked_out()
 #test_Class.test_login_empty_2()
-test_Class.test_valid_login()
+#test_Class.test_valid_login()
+#test_Class.test_add_cart()
+#test_Class.test_take_price_list()
