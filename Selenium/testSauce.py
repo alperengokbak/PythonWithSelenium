3881db2e69a2fa55ff78155548171fe76f95f005
Alperen Gökbak <swalperen3008@gmail.com>
2023-03-23 21:51:45 +0300

    Added test_remove_product() in testsauce

--- Selenium/testSauce.py
+++ Selenium/testSauce.py
@@ -78,7 +78,7 @@ class TestSauce:
         list_item = self.driver.find_elements(By.CLASS_NAME, "inventory_item")
         print(f"Number of Item: {len(list_item)}")
     
-    def test_add_cart(self):
+    def test_add_product(self):
         self.user_name.send_keys("standard_user")
         self.password.send_keys("secret_sauce")
         sleep(1)
@@ -95,6 +95,38 @@ class TestSauce:
         test_result = int(test_cart.text) > 0
         print(f"Test Result: {test_result}, Product count: {test_cart.text}")
         
+    def test_remove_product(self):
+        self.user_name.send_keys("standard_user")
+        self.password.send_keys("secret_sauce")
+        sleep(1)
+        login_btn = self.driver.find_element(By.CLASS_NAME, "submit-button")
+        login_btn.click()
+        sleep(1)
+        add_button = self.driver.find_element(By.XPATH, "//*[@id='add-to-cart-sauce-labs-bike-light']")
+        add_button.click()
+        sleep(1)
+        #add_button = self.driver.find_element(By.XPATH, "//*[@id='add-to-cart-sauce-labs-fleece-jacket']")
+        #add_button.click()
+        #sleep(1)
+        test_cart = self.driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+        test_cart.click()
+        sleep(1)
+        remove_button = self.driver.find_element(By.ID, "remove-sauce-labs-bike-light")
+        remove_button.click()
+        sleep(1)
+        cart_item_count = self.driver.find_elements(By.CLASS_NAME, "cart_item")
+        try: 
+            for element in cart_item_count:
+                print(element)
+        except Exception as e:
+            print(e)
+
+        test_result = cart_item_count == []
+        if test_result:
+            print(f"Test Result: {test_result}, Removed succesfully the whole products.")
+        else:
+            print(f"Test Result: {test_result}, The whole product did not remove from the cart.")
+
     def test_take_price_list(self):
         self.user_name.send_keys("standard_user")
         self.password.send_keys("secret_sauce")
@@ -118,5 +150,6 @@ test_Class = TestSauce()
 #test_Class.test_login_locked_out()
 #test_Class.test_login_empty_2()
 #test_Class.test_valid_login()
-#test_Class.test_add_cart()
+#test_Class.test_add_product()
+test_Class.test_remove_product()
 #test_Class.test_take_price_list()
